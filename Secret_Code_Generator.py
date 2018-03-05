585aec2edd3e284ea1c64a9549c66a91c2a4034e
NG <ngainer@ideaventionsacademy.org>
2018-03-05 16:04:00 -0500

    Done?

--- Secret_Code_Generator.py
+++ Secret_Code_Generator.py
@@ -4,45 +4,54 @@
 import string
 import random
 
+global alfbet
+global numbers
+
 global t
 global coded
-global ncoded
 global nrword
 global encode
 global randnum
+global mcoded
+
+# https://stackoverflow.com/questions/16060899/alphabet-range-python
+alfbet = list(2*string.ascii_lowercase)
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 t = ''
 coded = ''
-ncoded = ''
 nrword = ''
 encode = ''
-randnum = random.randint(2,100)
+mcoded = ''
+
+randnum = random.randint(2,45)
+while randnum == 26:
+    randnum = random.randint(2,45)
    
 def secret_code():
     global t
     global coded
-    global ncoded
     global randnum
     global encode
     global nrword
+    global mcoded
+    
     t = input('Enter a message to be encoded : ')
     encode = input('Now enter how you would like your message to be encoded in. You may choose more than one. c = Cypher; r = letter rearrangement; m = morse code : ')
 
     for l in encode:
         if l == 'c':
+            Cypher()
             if len(encode) == 1:
-                Cypher()
                 print(coded)
-            else:
-                Cypher()
+
         elif l == 'r':
             Rearrange()
+            if 'm' not in encode:
+                print(nrword)
         elif l == 'm':
             Morsecode()
-            if len(encode) == 1:
-                print(coded)
-            else:
-                print(ncoded)
+            print(mcoded)
         else:
             print('invalid encoder')
     if 'c' in encode:
@@ -51,49 +60,57 @@ def secret_code():
 
 # Cypher
 def Cypher():
+    global alfbet
     global t
     global coded
     global randnum
 
     t = t.lower()
+    coded = coded.lower()
     
-    # https://stackoverflow.com/questions/16060899/alphabet-range-python
-    alfbet = list(2*string.ascii_lowercase)
-
     for l in t:
         if l == ' ':
             coded = coded + ' '
+        elif l in numbers:
+            coded = coded + l
+        elif l not in alfbet and l not in numbers:
+            l = ''
         else:
             # https://stackoverflow.com/questions/43102861/finding-an-element-from-one-list-in-another-list-in-python
             i = alfbet.index(l)
-            
+                
             i = int(i) + 5
             c = alfbet[i]
             c = str(c)
             coded = coded + (c)
-            
+
 
 # rearrangement
 def Rearrange():
     global t
     global coded
-    global ncoded
     global nrword
-    if len(ncoded) > 0:
-        coded = ncoded
+    
+    t = t.lower()
+    coded = coded.lower()
+    nrword = nrword.lower()
+
 
     t = t + ' '
-    
     word = ''
+    
     for c in t:
         if c != ' ':
-            word = str(word)
-            
-            word = word.replace("[","")
-            word = word.replace("]","")
+            if c in alfbet or c in numbers:    
+                word = str(word)
                 
-            c = str(c)
-            word = word + (c)
+                word = word.replace("[","")
+                word = word.replace("]","")
+                    
+                c = str(c)
+                word = word + (c)
+            else:
+                c = ''
         else:
             for l in range(len(word)):
                 # https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
@@ -109,177 +126,260 @@ def Rearrange():
                 
                 del word[letind]
             nrword = nrword + ' '
+        if nrword == t:
+            nrword = ''
+            Rearrange()
 
     word = str(word)
-    print(nrword)
                     
                                           
 
-
 # morsecode
 def Morsecode():
+    global nrword
     global coded
     global encode
-    global ncoded
     global t
+    global mcoded
+    nrword = nrword.lower()
+    coded = coded.lower()
+    encode = encode.lower()
     t = t.lower()
-    if len(encode) == 1:
+    mcoded = mcoded.lower()
+    if len(nrword) > 0:
+        for l in nrword:
+            if l == ' ':
+                mcoded = mcoded + '  '
+            elif l == 'a':
+                mcoded = mcoded + '●- '
+            elif l == 'b':
+                mcoded = mcoded + '-●●● '
+            elif l == 'c':
+                mcoded = mcoded + '-●-● '
+            elif l == 'd':
+                mcoded = mcoded + '-●● '
+            elif l == 'e':
+                mcoded = mcoded + '● '
+            elif l == 'f':
+                mcoded = mcoded + '●●-● '
+            elif l == 'g':
+                mcoded = mcoded + '--● '
+            elif l == 'h':
+                mcoded = mcoded + '●●●● '
+            elif l == 'i':
+                mcoded = mcoded + '●● '
+            elif l == 'j':
+                mcoded = mcoded + '●--- '
+            elif l == 'k':
+                mcoded = mcoded + '-●- '
+            elif l == 'l':
+                mcoded = mcoded + '●-●● '
+            elif l == 'm':
+                mcoded = mcoded + '-- '
+            elif l == 'n':
+                mcoded = mcoded + '-● '
+            elif l == 'o':
+                mcoded = mcoded + '--- '
+            elif l == 'p':
+                mcoded = mcoded + '●--● '
+            elif l == 'q':
+                mcoded = mcoded + '--●- '
+            elif l == 'r':
+                mcoded = mcoded + '●-● '
+            elif l == 's':
+                mcoded = mcoded + '●●● '
+            elif l == 't':
+                mcoded = mcoded + '- '
+            elif l == 'u':
+                mcoded = mcoded + '●●- '
+            elif l == 'v':
+                mcoded = mcoded + '●●●- '
+            elif l == 'w':
+                mcoded = mcoded + '●-- '
+            elif l == 'x':
+                mcoded = mcoded + '-●●- '
+            elif l == 'y':
+                mcoded = mcoded + '-●-- '
+            elif l == 'z':
+                mcoded = mcoded + '--●● '
+            elif l == '1':
+                mcoded = mcoded + '●---- '
+            elif l == '2':
+                mcoded = mcoded + '●●--- '
+            elif l == '3':
+                mcoded = mcoded + '●●●-- '
+            elif l == '4':
+                mcoded = mcoded + '●●●●- '
+            elif l == '5':
+                mcoded = mcoded + '●●●●● '
+            elif l == '6':
+                mcoded = mcoded + '-●●●● '
+            elif l == '7':
+                mcoded = mcoded + '--●●● '
+            elif l == '8':
+                mcoded = mcoded + '---●● '
+            elif l == '9':
+                mcoded = mcoded + '----● '
+            elif l == '0':
+                mcoded = mcoded + '----- '
+            else:
+                mcoded = mcoded + '? '
+    elif len(encode) == 1:
         for l in t:
             if l == ' ':
-                coded = coded + '  '
+                mcoded = mcoded + '  '
             elif l == 'a':
-                coded = coded + '●- '
+                mcoded = mcoded + '●- '
             elif l == 'b':
-                coded = coded + '-●●● '
+                mcoded = mcoded + '-●●● '
             elif l == 'c':
-                coded = coded + '-●-● '
+                mcoded = mcoded + '-●-● '
             elif l == 'd':
-                coded = coded + '-●● '
+                mcoded = mcoded + '-●● '
             elif l == 'e':
-                coded = coded + '● '
+                mcoded = mcoded + '● '
             elif l == 'f':
-                coded = coded + '●●-● '
+                mcoded = mcoded + '●●-● '
             elif l == 'g':
-                coded = coded + '--● '
+                mcoded = mcoded + '--● '
             elif l == 'h':
-                coded = coded + '●●●● '
+                mcoded = mcoded + '●●●● '
             elif l == 'i':
-                coded = coded + '●● '
+                mcoded = mcoded + '●● '
             elif l == 'j':
-                coded = coded + '●--- '
+                mcoded = mcoded + '●--- '
             elif l == 'k':
-                coded = coded + '-●- '
+                mcoded = mcoded + '-●- '
             elif l == 'l':
-                coded = coded + '●-●● '
+                mcoded = mcoded + '●-●● '
             elif l == 'm':
-                coded = coded + '-- '
+                mcoded = mcoded + '-- '
             elif l == 'n':
-                coded = coded + '-● '
+                mcoded = mcoded + '-● '
             elif l == 'o':
-                coded = coded + '--- '
+                mcoded = mcoded + '--- '
             elif l == 'p':
-                coded = coded + '●--● '
+                mcoded = mcoded + '●--● '
             elif l == 'q':
-                coded = coded + '--●- '
+                mcoded = mcoded + '--●- '
             elif l == 'r':
-                coded = coded + '●-● '
+                mcoded = mcoded + '●-● '
             elif l == 's':
-                coded = coded + '●●● '
+                mcoded = mcoded + '●●● '
             elif l == 't':
-                coded = coded + '- '
+                mcoded = mcoded + '- '
             elif l == 'u':
-                coded = coded + '●●- '
+                mcoded = mcoded + '●●- '
             elif l == 'v':
-                coded = coded + '●●●- '
+                mcoded = mcoded + '●●●- '
             elif l == 'w':
-                coded = coded + '●-- '
+                mcoded = mcoded + '●-- '
             elif l == 'x':
-                coded = coded + '-●●- '
+                mcoded = mcoded + '-●●- '
             elif l == 'y':
-                coded = coded + '-●-- '
+                mcoded = mcoded + '-●-- '
             elif l == 'z':
-                coded = coded + '--●● '
+                mcoded = mcoded + '--●● '
             elif l == '1':
-                coded = coded + '●---- '
+                mcoded = mcoded + '●---- '
             elif l == '2':
-                coded = coded + '●●--- '
+                mcoded = mcoded + '●●--- '
             elif l == '3':
-                coded = coded + '●●●-- '
+                mcoded = mcoded + '●●●-- '
             elif l == '4':
-                coded = coded + '●●●●- '
+                mcoded = mcoded + '●●●●- '
             elif l == '5':
-                coded = coded + '●●●●● '
+                mcoded = mcoded + '●●●●● '
             elif l == '6':
-                coded = coded + '-●●●● '
+                mcoded = mcoded + '-●●●● '
             elif l == '7':
-                coded = coded + '--●●● '
+                mcoded = mcoded + '--●●● '
             elif l == '8':
-                coded = coded + '---●● '
+                mcoded = mcoded + '---●● '
             elif l == '9':
-                coded = coded + '----● '
+                mcoded = mcoded + '----● '
             elif l == '0':
-                coded = coded + '----- '
+                mcoded = mcoded + '----- '
             else:
-                coded = coded + '? '
+                mcoded = mcoded + '? '
     else:
         for l in coded:
-            global ncoded
             if l == ' ':
-                ncoded = ncoded + '  '
+                mcoded = mcoded + '  '
             elif l == 'a':
-                ncoded = ncoded + '●- '
+                mcoded = mcoded + '●- '
             elif l == 'b':
-                ncoded = ncoded + '-●●● '
+                mcoded = mcoded + '-●●● '
             elif l == 'c':
-                ncoded = ncoded + '-●-● '
+                mcoded = mcoded + '-●-● '
             elif l == 'd':
-                ncoded = ncoded + '-●● '
+                mcoded = mcoded + '-●● '
             elif l == 'e':
-                ncoded = ncoded + '● '
+                mcoded = mcoded + '● '
             elif l == 'f':
-                ncoded = ncoded + '●●-● '
+                mcoded = mcoded + '●●-● '
             elif l == 'g':
-                ncoded = ncoded + '--● '
+                mcoded = mcoded + '--● '
             elif l == 'h':
-                ncoded = ncoded + '●●●● '
+                mcoded = mcoded + '●●●● '
             elif l == 'i':
-                ncoded = ncoded + '●● '
+                mcoded = mcoded + '●● '
             elif l == 'j':
-                ncoded = ncoded + '●--- '
+                mcoded = mcoded + '●--- '
             elif l == 'k':
-                ncoded = ncoded + '-●- '
+                mcoded = mcoded + '-●- '
             elif l == 'l':
-                ncoded = ncoded + '●-●● '
+                mcoded = mcoded + '●-●● '
             elif l == 'm':
-                ncoded = ncoded + '-- '
+                mcoded = mcoded + '-- '
             elif l == 'n':
-                ncoded = ncoded + '-● '
+                mcoded = mcoded + '-● '
             elif l == 'o':
-                ncoded = ncoded + '--- '
+                mcoded = mcoded + '--- '
             elif l == 'p':
-                ncoded = ncoded + '●--● '
+                mcoded = mcoded + '●--● '
             elif l == 'q':
-                ncoded = ncoded + '--●- '
+                mcoded = mcoded + '--●- '
             elif l == 'r':
-                ncoded = ncoded + '●-● '
+                mcoded = mcoded + '●-● '
             elif l == 's':
-                ncoded = ncoded + '●●● '
+                mcoded = mcoded + '●●● '
             elif l == 't':
-                ncoded = ncoded + '- '
+                mcoded = mcoded + '- '
             elif l == 'u':
-                ncoded = ncoded + '●●- '
+                mcoded = mcoded + '●●- '
             elif l == 'v':
-                ncoded = ncoded + '●●●- '
+                mcoded = mcoded + '●●●- '
             elif l == 'w':
-                ncoded = ncoded + '●-- '
+                mcoded = mcoded + '●-- '
             elif l == 'x':
-                ncoded = ncoded + '-●●- '
+                mcoded = mcoded + '-●●- '
             elif l == 'y':
-                ncoded = ncoded + '-●-- '
+                mcoded = mcoded + '-●-- '
             elif l == 'z':
-                ncoded = ncoded + '--●● '
+                mcoded = mcoded + '--●● '
             elif l == '1':
-                ncoded = ncoded + '●---- '
+                mcoded = mcoded + '●---- '
             elif l == '2':
-                ncoded = ncoded + '●●--- '
+                mcoded = mcoded + '●●--- '
             elif l == '3':
-                ncoded = ncoded + '●●●-- '
+                mcoded = mcoded + '●●●-- '
             elif l == '4':
-                ncoded = ncoded + '●●●●- '
+                mcoded = mcoded + '●●●●- '
             elif l == '5':
-                ncoded = ncoded + '●●●●● '
+                mcoded = mcoded + '●●●●● '
             elif l == '6':
-                ncoded = ncoded + '-●●●● '
+                mcoded = mcoded + '-●●●● '
             elif l == '7':
-                ncoded = ncoded + '--●●● '
+                mcoded = mcoded + '--●●● '
             elif l == '8':
-                ncoded = ncoded + '---●● '
+                mcoded = mcoded + '---●● '
             elif l == '9':
-                ncoded = ncoded + '----● '
+                mcoded = mcoded + '----● '
             elif l == '0':
-                ncoded = ncoded + '----- '
+                mcoded = mcoded + '----- '
             else:
-                ncoded = ncoded + '? '
+                mcoded = mcoded + '? '
 
 # Runs starting function
 secret_code()
